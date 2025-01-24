48e0991284af0faeaf0c0edc72706fccc46e335a
varad-pawar1 <varadpawarm@gmail.com>
2025-01-24 19:10:27 +0530

    https://github.com/varad-pawar1/Master-Union.git

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
+
+menuToggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+
+
